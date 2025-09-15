b796de8a1c221e06c02b30351da7b619b51e1653
luishcarreira <luishenriquecarreira@gmail.com>
2025-09-15 14:28:51 -0300

    ajustando ambiente

--- src/services/httpClient.ts
+++ src/services/httpClient.ts
@@ -602,5 +602,5 @@ export class HttpClient {
 }
 
 export const httpClient = new HttpClient(
-  import.meta.env.VITE_API_URL || "https://api.nexusvitally.com.br"
+  import.meta.env.VITE_API_URL || "https://apihomol.nexusvitally.com.br"
 );
