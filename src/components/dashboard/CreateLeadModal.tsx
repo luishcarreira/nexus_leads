1c2b5f4511d5ae9563d75b5cdcea6798807a3dd6
luishcarreira <luishenriquecarreira@gmail.com>
2026-04-27 10:28:56 -0300

    adicionando nova opção de origem lead

--- src/components/dashboard/CreateLeadModal.tsx
+++ src/components/dashboard/CreateLeadModal.tsx
@@ -111,6 +111,7 @@ export const CreateLeadModal: React.FC<CreateLeadModalProps> = ({
     "Whatsapp Vitally Orgânico",
     "Google",
     "Facebook",
+    "Feira"
   ];
 
   const handleInputChange = (
